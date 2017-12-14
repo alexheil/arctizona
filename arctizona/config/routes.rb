2b4b6fd7789718f5c5e11d4215ca971c87eef1a5
alexheil <aheil4250@gmail.com>
2017-12-13 21:20:01 -0500

    routes

--- arctizona/config/routes.rb
+++ arctizona/config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+
+  get 'privacy' => 'static_pages#privacy'
+  get 'terms' => 'static_pages#terms'
+  get 'about' => 'static_pages#about'
+
+  devise_for :users, controllers: { sessions: "users/sessions", passwords: "users/passwords", registrations: "users/registrations", confirmations: "users/confirmations",  unlocks: "users/unlocks"}
+
+  devise_scope :user do
+    get "sign_out", to: "users/sessions#destroy"
+  end
+
+  resources :users, controller: 'users/users', only: :show
+
 end
