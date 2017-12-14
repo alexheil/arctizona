811441a1c89f49ed193d8df0d7252c6266653216
alexheil <aheil4250@gmail.com>
2017-12-14 10:19:23 -0500

    login

--- arctizona/app/controllers/application_controller.rb
+++ arctizona/app/controllers/application_controller.rb
@@ -6,8 +6,9 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+      added_attrs = [:username, :email, :password, :password_confirmation, :current_password, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     end
 
 end
