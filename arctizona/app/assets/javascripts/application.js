5a14dae953123a9e546f96cb19e32b78ca39ecc4
alexheil <aheil4250@gmail.com>
2017-12-28 12:22:33 -0500

    design

--- arctizona/app/assets/javascripts/application.js
+++ arctizona/app/assets/javascripts/application.js
@@ -9,12 +9,32 @@ var editProfile = function() {
     $('#profile-form').show(300);
     $('#close-profile').show();
     $('#edit-profile').hide();
+    $('#back').show();
+    $(".wrapper").addClass("blur");
   });
 
   $('#close-profile').click(function() {
     $('#profile-form').hide(300);
     $('#close-profile').hide();
     $('#edit-profile').show();
+    $('#back').hide();
+    $(".wrapper").removeClass("blur");
+  });
+
+  $('#back').click(function() {
+    $('#profile-form').hide(300);
+    $('#close-profile').hide();
+    $('#edit-profile').show();
+    $('#back').hide();
+    $(".wrapper").removeClass("blur");
+  });
+
+  $('#close').click(function() {
+    $('#profile-form').hide(300);
+    $('#close-profile').hide();
+    $('#edit-profile').show();
+    $('#back').hide();
+    $(".wrapper").removeClass("blur");
   });
 
 };
