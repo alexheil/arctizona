680744579a0b323c97d5a721fbbf557b471ef8a5
alexheil <aheil4250@gmail.com>
2018-01-09 11:23:10 -0500

    payment settings

--- arctizona/app/models/user.rb
+++ arctizona/app/models/user.rb
@@ -22,6 +22,7 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_one :theme, dependent: :destroy
   has_one :location, dependent: :destroy
+  has_one :payment_setting, dependent: :destroy
 
   before_save :should_generate_new_friendly_id?, if: :username_changed?
   before_save :downcase_username
