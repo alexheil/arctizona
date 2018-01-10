80b123818814947ff090dee99f64ae91b246412e
alexheil <aheil4250@gmail.com>
2018-01-10 17:24:18 -0500

    payment settings

--- arctizona/app/models/payment_setting.rb
+++ arctizona/app/models/payment_setting.rb
@@ -7,5 +7,5 @@ class PaymentSetting < ApplicationRecord
   validates :month, presence: true, length: { maximum: 2 }, numericality: { less_than_or_equal_to: 12, greater_than: 0}
   validates :day, presence: true, length: { maximum: 2 }, numericality: { less_than_or_equal_to: 31, greater_than: 0 }
   validates :year, presence: true, length: { is: 4 }, numericality: { less_than_or_equal_to: 2015, greater_than: 1940}
-  validates :country, presence: true
+  #validates :country, presence: true
 end
