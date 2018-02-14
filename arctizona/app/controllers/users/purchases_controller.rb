f9fe28bb5014a042c2e7826a86a883ca05eb9f88
alexheil <aheil4250@gmail.com>
2018-02-14 10:39:50 -0500

    purchases

--- arctizona/app/controllers/users/purchases_controller.rb
+++ arctizona/app/controllers/users/purchases_controller.rb
@@ -12,6 +12,7 @@ class Users::PurchasesController < ApplicationController
   private 
 
     def purchase_params
+      params.require(:purchase).permit(:buyer_id, :seller_id, :stripe_charge_id, :quantity, :pay_your_own_price, :complete_price, :full_name, :street_address, :city, :state, :country, :zip_code, :is_shipped)
     end
 
 end
