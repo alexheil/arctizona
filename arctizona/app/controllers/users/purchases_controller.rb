ae61fbcbf6af537935a6388f688ee64dfcf0727e
alexheil <aheil4250@gmail.com>
2018-02-14 10:33:47 -0500

    purchases

--- arctizona/app/controllers/users/purchases_controller.rb
+++ arctizona/app/controllers/users/purchases_controller.rb
@@ -1,3 +1,17 @@
 class Users::PurchasesController < ApplicationController
 
+  def create
+    @user = User.friendly.find(params[:user_id])
+    @photo = Photo.friendly.find(params[:photo_id])
+    @purchase = @photo.purchases.build(purchase_params)
+
+    @purchase.buyer_id = current_user.id
+    @purchase.seller_id = @user.id
+  end
+
+  private 
+
+    def purchase_params
+    end
+
 end
